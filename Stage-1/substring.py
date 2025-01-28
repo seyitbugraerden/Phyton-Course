d167f519e8b4be665ece728e25a2ba0dd4706c5c
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-28 23:44:26 +0300

    substring

---
+++ Stage-1/substring.py
@@ -0,0 +1,11 @@
+mesaj = "Deneme"
+
+print(mesaj[0]) # 0.index değerini al
+
+print(mesaj[-1]) # sonuncu index değerini al
+
+print(mesaj[2:]) # 2.indexten sonra ki değerleri al
+
+print(mesaj[2:4]) # 2. ve 4. index arasındaki değerleri al
+
+print(mesaj[:2]) # 2. indexe kadar olan değerleri al
