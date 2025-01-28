ad6b6f068ba40f2abdc1d49262eeefc6a3a9e2bf
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-28 23:32:53 +0300

    Degiskenler

---
+++ Stage-1/degiskenler.py
@@ -0,0 +1,13 @@
+sayi = 10 #integer
+floatSayi = 10.0 #float
+
+#float değer ".0" gibi kullanım gerektirir. Sonunda ".0" kullanılmazsa integer olarak çalışır.
+
+deger = "10" #string
+tc = "12345678901"
+
+# print(sayi + 10) # integer + integer
+# print(deger + 10) # string + integer => Çalışmaz.
+
+# print(float(tc) + 11) # string değer üzerinde tip değişimi ile başarılı sonuç
+print(type(deger))
