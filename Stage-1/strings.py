154660209d34a9d002aa65cabee02ef08bd610c3
seyitbugraerden <seyitbugraerden@gmail.com>
2025-01-28 23:39:46 +0300

    strings

---
+++ Stage-1/strings.py
@@ -0,0 +1,12 @@
+firstString = "Phyton Test 1"
+
+secondString = "Phyton Test 2"
+
+# print(firstString.upper()) String'i büyük harfle print alabiliriz
+
+if firstString.upper() == secondString.upper():
+    print("Değerler Aynı")
+else:
+    print("Değerler Farklı")
+
+    
