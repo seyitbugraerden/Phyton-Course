5d8f809da2ce9959b2940bec05d400ff833f4e0c
seyitbugraerden <seyitbugraerden@gmail.com>
2025-02-02 23:54:18 +0300

    test

--- Stage-1/input.py
+++ Stage-1/input.py
@@ -2,4 +2,4 @@ ad = input('Adınız')
 sayi1 = input('Sayı 1 Giriniz')
 sayi2 = input('Sayı 2 Giriniz')
 
-print(int(sayi1) + int(sayi2)) #int ile string değeri integer haline getirebiliyoruz.
+print(int(sayi1) + int(sayi2)) #int ile string değeri integer haline getirebiliyoruz. test
