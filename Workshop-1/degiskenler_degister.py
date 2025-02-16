6f462e2f311de69b051854e890dafbd88003febb
seyitbugraerden <seyitbugraerden@gmail.com>
2025-02-16 19:14:29 +0300

    states

---
+++ Workshop-1/degiskenler_degister.py
@@ -0,0 +1,14 @@
+x = 10 #integer
+y = 20 #integer
+
+# print("x = " + str(x))
+# print("y = " + str(y))
+# print("x + y = " + str(int(x) + int(y)))
+
+# temp = x
+# t = x + y
+
+# print(t)
+
+x,y = y,x
+print(x,y)
